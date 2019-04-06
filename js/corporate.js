16a1b2c5e3084dca3005642eb36b0e49db04d638
Ptashkina Liliya <efekta9@gmail.com>
2019-04-06 11:40:09 +0300

    no message

--- js/corporate.js
+++ js/corporate.js
@@ -1,7 +1,55 @@
 $(document).ready(function($){
 
 
-$(".fancybox").fancybox();
+	$(".fancybox").fancybox();
 
+	$('.reviews').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    nav: true,
+        // navText: ["<img src='img/icons/arrow.png'>", "<img src='img/icons/arrow.png'>"],
+	    responsiveClass:true,
+	    responsive:{
+	        0:{
+	            items:1,
+	            nav:true
+	        },
+	        600:{
+	            items:3,
+	            nav:false
+	        },
+	        1000:{
+	            items:3,
+	            nav:true,
+	            loop:false
+	        }
+	    }
+	})
 
+	// accordion
+	$('.set > span').on('click', function() {
+	    if ($(this).hasClass("active")) {
+	      $(this).removeClass("active");
+	      $(this)
+	        .siblings(".accordion-content")
+	        .slideUp(200);
+	      $(".set > span i")
+	        .removeClass("fa-minus")
+	        .addClass("fa-plus");
+	    } else {
+	      $(".set > span i")
+	        .removeClass("fa-minus")
+	        .addClass("fa-plus");
+	      $(this)
+	        .find("i")
+	        .removeClass("fa-plus")
+	        .addClass("fa-minus");
+	      $('.set > span').removeClass('active');
+	      $(this).addClass("active");
+	      $('.accordion-content').slideUp(200);
+	      $(this)
+	        .siblings('.accordion-content')
+	        .slideDown(200);
+	    }
+	  });
 })
